f80eae747de19ddfda2e9e11434375b4ec8056d3
Eitan Herman <eitan@securegion.com>
2017-06-18 20:47:29 +0300

    minor fixes

--- server/src/test/java/com/securegion/ServerApplicationTests.java
+++ server/src/test/java/com/securegion/ServerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ProjectserverApplicationTests {
+public class ServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
